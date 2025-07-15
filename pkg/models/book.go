dcdff869141a7bacd91d8b7f6b9054571df139fa
Dharm Singh <dharmhsing@gmail.com>
2025-07-15 19:40:41 +0530

    build routes

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"github.com/dharmvr1/go-bookStore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+}
--- pkg/routes/bookstore-route.go
+++ pkg/routes/bookstore-route.go
@@ -1,6 +1,16 @@
 package routes
 
+import (
+	"github.com/dharmvr1/go-bookStore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
 
-import {
-	
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.Handle("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
 }
